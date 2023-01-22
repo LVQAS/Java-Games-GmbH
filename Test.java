fb128e1e10e3bb3e38147d9d2ab533f2e714648d
LVQAS <123311437+LVQAS@users.noreply.github.com>
2023-01-22 15:47:24 +0100

    Create Test.java
    Der aktuelle Code für die Datei 'Test.java'

---
+++ Test.java
@@ -0,0 +1,30 @@
+public class Test {
+	
+	//Hauptmethode: Sie ist zuständig für die Ausführbarkeit
+	
+	public static void main (String[] args) {
+		
+		Spielekonsole alpha = new Spielekonsole();
+		Spieler sp1 = new Spieler();
+
+		alpha.hello();
+
+		sp1.setName();
+		System.out.println();
+
+		sp1.setAlter();
+		System.out.println();
+
+		sp1.setGeschlecht();
+		System.out.println();
+
+		sp1.setGroesse();
+		System.out.println();
+		
+		System.out.println("Wilkommen " + sp1.name + "!");
+
+		alpha.goodbye();
+		
+	}
+
+}
