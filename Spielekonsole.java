ec3baeb0d0579056b509ab0efbba5df3cc0dc9e9
LVQAS <123311437+LVQAS@users.noreply.github.com>
2023-01-22 15:49:15 +0100

    Create Spielekonsole.java

---
+++ Spielekonsole.java
@@ -0,0 +1,12 @@
+
+public class Spielekonsole
+{
+	
+	public void hello() {
+		System.out.println("Starten der JavaGames GmbH Spielekonsole erfolgreich!");
+	}
+		
+	public void goodbye() {
+		System.out.println("Bis Baldrian!");
+	}
+}
