b865314892ae6f651112b3cb4d6b0cf6ebbd29b6
Lukas Belz <belz@singhammer.com>
2023-05-15 15:13:11 +0200

    Add game GuN

---
+++ GuN.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class GuN {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int Zufallszahl = random.nextInt(6) + 1; // generiere eine Zufallszahl zwischen 1 und 6
+        System.out.println("Die Zahl ist: " + Zufallszahl);
+        if (Zufallszahl == 6) {
+            System.out.println("Gewinn");
+        } else {
+            System.out.println("Niete");
+        }
+    }
+}
